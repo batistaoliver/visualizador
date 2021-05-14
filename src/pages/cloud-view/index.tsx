22fab20243d2fae517fc8d5f58f79ab55b4009ed
batista <batistaoliveiradev@gmail.com>
2021-05-13 21:29:36 -0300

    move edit table to preview screen.

--- src/pages/cloud-view/index.tsx
+++ src/pages/cloud-view/index.tsx
@@ -6,8 +6,20 @@ import { Button } from 'react-bootstrap'
 import BasicScene from "components/BasicScene"
 import LoadingSpinner from 'components/LoadingSpinner'
 import SceneControls from 'pages/cloud-view/SceneControls'
+import Preview from 'components/Preview'
+import Table from 'react-bootstrap/Table'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 import { apiURL } from 'utils'
 import styles from './index.scss'
+import {
+  Trash as TrashIcon,
+  Pencil as PencilIcon,
+} from 'react-bootstrap-icons'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Modal from 'react-bootstrap/Modal'  
+import Dropdown from 'react-bootstrap/Dropdown'
 
 type State = {
   isLoading: boolean
@@ -17,6 +29,8 @@ type State = {
   mouseInteraction: boolean
   showAxes: boolean
   showOriginalCopy: boolean
+  clouds: Cloud[]
+  deleteID: Nullable<string>
 }
 
 type Props = {
@@ -26,14 +40,16 @@ type Props = {
 const DEFAULT_SCALE = 4
 
 export default class CloudView extends React.PureComponent<Props, State> {
-  loader: PCDLoader = new PCDLoader()
   state: State = {
     isLoading: false,
     mouseInteraction: false,
     showAxes: true,
     showOriginalCopy: false,
+    clouds: [],
+    deleteID: null
   }
-
+  loader: PCDLoader = new PCDLoader()
+  
   sceneContainer: RefObject<HTMLDivElement> = createRef()
 
   componentDidMount() {
@@ -44,8 +60,44 @@ export default class CloudView extends React.PureComponent<Props, State> {
     }).then((response) => {
       this.loader.load(apiURL(response.data.url), this.onLoad, this.onLoadProgress, this.onLoadError)
     })
+
+    axios({
+      url: apiURL('/api/point-clouds'),
+    })
+      .then((response) => {
+        this.setState({ clouds: response.data })
+      })
+      .catch((response) => {
+        console.log(response)
+      })
   }
 
+  confirmDelete = () => {
+    const { deleteID } = this.state
+    axios({
+      method: 'DELETE',
+      url: apiURL(`/api/point-clouds/${deleteID}`),
+    })
+      .then(response => {
+        console.log(response)
+        this.setState((state: State) => ({
+          clouds: state.clouds.filter(cloud => cloud.id !== deleteID),
+        }), () => {
+          this.hideDeleteModal()
+          alert('Nuvem deletada com sucesso')
+        })
+      })
+      .catch(response => {
+        console.log(response)
+        alert('Erro ao deletar nuvem')
+      })
+  }
+
+  showDeleteModal = (deleteID: string) => this.setState({ deleteID })
+
+  hideDeleteModal = () => this.setState({ deleteID: null })
+
+  
   onLoad = (mesh: Points) => {
     mesh.scale.fromArray([DEFAULT_SCALE, DEFAULT_SCALE, DEFAULT_SCALE])
     this.setState({
@@ -137,26 +189,92 @@ export default class CloudView extends React.PureComponent<Props, State> {
     )
   }
 
+  
   render() {
     const { mesh } = this.state
-
+    
     return (
-      <div className={styles.page}>
-        <header>
-          <h1 className={styles.h1}>View Point Cloud</h1>
-          <Button className="float-right btn-sm" children="Voltar" href="/clouds" />
-        </header>
-        {this.renderSceneContainer()}
-        <div className={styles.footer}>
-          {mesh && (
-            <SceneControls
-              mesh={mesh}
-              onUpdate={this.rerender}
-              onChange={this.onControlChange}
-            />
-          )}
-        </div>
-      </div>
+      
+      <Container className={styles.container}>
+        <Row>
+          <Col xs={2} className="mt-4">
+            <Table striped bordered hover id='table-clouds' className={styles.table}> 
+              <thead className={styles.thead}>
+                {this.renderTableHeader()}
+              </thead>
+              <tbody className={styles.scrollTable}>
+                {this.renderTableData()}
+              </tbody> 
+            </Table>
+            <Button className={styles.includeBtn} variant="primary" href="/clouds/insert" size="sm">
+                Insert New
+              </Button>
+              {this.renderDeleteModal()}
+          </Col>
+          <Col xs={10}>
+            <div className={styles.page}> 
+              <header>
+                <h1 className={styles.h1}>View Point Cloud</h1>
+                {/* <Button className="float-right btn-sm" children="Voltar" href="/clouds" /> */}
+              </header>
+              {this.renderSceneContainer()}
+              <div id='footer' className={styles.footer}>
+                {mesh && (
+                <SceneControls
+                  mesh={mesh}
+                  onUpdate={this.rerender}
+                  onChange={this.onControlChange}
+                />
+                )}
+              </div> 
+            </div>
+          </Col> 
+        </Row> 
+      </Container> 
     )
   }
+
+  renderTableHeader = () => (
+    <tr id='table-header'>
+      <th className={styles.previewCol}>Preview</th>
+    </tr>
+  )
+
+  renderTableData = () => {
+    return this.state.clouds.map(cloud => {
+      const { id, name, url } = cloud
+
+      return (
+        <tr key={id}>
+          <td>
+            <Preview url={apiURL(url)}/>
+            <ButtonGroup claaria-label="Basic example">
+              <Button className={styles.actionBtn} href={`/clouds/edit/${id}`} variant="link" size="sm" title="Click to Edit">
+                <PencilIcon className={styles.action}/>
+              </Button>
+              <Button className={styles.actionBtn} variant="link" onClick={() => this.showDeleteModal(id)} title="Click to Delete">
+                <TrashIcon className={styles.action}/>
+              </Button>
+            </ButtonGroup>
+          </td> 
+        </tr>
+      )
+    })
+  }
+  renderDeleteModal = () => (
+    <Modal show={this.state.deleteID !== null}>
+      <Modal.Header>
+        <Modal.Title>Deletar Nuvem</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Confirma a exclusão?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={this.hideDeleteModal}>
+          Não
+        </Button>
+        <Button variant="primary" onClick={this.confirmDelete}>
+          Sim
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
 }
